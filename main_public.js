5e5a359215ed2c405b4a0cf7888d8c32de295061
dkornel01 <115635103+dkornel01@users.noreply.github.com>
2023-04-18 20:16:30 +0200

    torles_marad

--- main_public.js
+++ main_public.js
@@ -7,16 +7,55 @@ function init(lista){
     let cards=card_maker(OBJEKTUMLISTA);
     const ARTICLEELEM=$("article");
     ARTICLEELEM.html(cards);
-    const FOOTERELEM=$("footer")
-    $("#nagyitas").click(function(){
-        console.log("volt")
-        let card=event.target.footer
-        console.log(card)
-        let tarto=nagyitas(card)
-        FOOTERELEM.html(tarto);
+    const FOOTERELEM=$("footer");
+    let card
+    $("#nagyitas0").click(function(){
+        card=event.target.name
+        nagyitas(card,ARTICLEELEM)
+        select(card,ARTICLEELEM)
+    })
+    $("#nagyitas1").click(function(){
+        card=event.target.name
+        nagyitas(card,ARTICLEELEM)
+        select(card,ARTICLEELEM)      
+    })
+    $("#nagyitas2").click(function(){
+        card=event.target.name
+        nagyitas(card,ARTICLEELEM)
+        select(card,ARTICLEELEM)       
+    })
+    $("#nagyitas3").click(function(){
+        card=event.target.name
+        nagyitas(card,ARTICLEELEM)
+        select(card,ARTICLEELEM)       
+    })
+    $("#nagyitas4").click(function(){
+        card=event.target.name
+        nagyitas(card,ARTICLEELEM)
+        select(card,ARTICLEELEM)   
     })
 
 };
+function select(card,ARTICLEELEM){
+    $("#job").click(function(){
+        card=parseInt(card)
+        if ((card+1)<5){
+            card=(card+1)
+            nagyitas(card,ARTICLEELEM)
+            select(card,ARTICLEELEM)
+        }
+    })
+    $("#bal").click(function(){
+        if ((card-1)>-1){
+        card=(card-1)
+        nagyitas(card,ARTICLEELEM)
+        }
+        select(card,ARTICLEELEM)
+    })
+    $("#alap").click(function(){
+        init()
+    })
+}
 function card_maker(OBJEKTUMLISTA){
     let txt="<div class='container-fluid mt-3'>";
     txt +="<h2>'Kutyák'</h2>";
@@ -36,7 +75,7 @@ function card_maker(OBJEKTUMLISTA){
                 txt+=`<div class="card-body">${object[key]}</div>`;
             }
         }
-        txt+=`<div class="card-footer" id=nagyitas><input type="button" value="nagyitas" name=${szamolo}></div>`;
+        txt+=`<div class="card-footer" id=nagyitas${szamolo}><input type="button" value="nagyitas" name=${szamolo}></div>`;
         szamolo=szamolo+1;
         console.log(szamolo)
         txt+='</div>';
@@ -46,18 +85,25 @@ function card_maker(OBJEKTUMLISTA){
     txt+="</div>";
     return txt
 }
-function nagyitas(card){
-    let txt=`<div class="card bg-secondary text-white">`;
-    const object=OBJEKTUMLISTA[card]
-        for (const key in object) {
-            if (key === "nev"){
-                txt+=`<div class="card-header">${object[key]}</div>`;
+function nagyitas(card,ARTICLEELEM){
+        let txt=`<div class="card bg-secondary text-white">`;
+        const object=OBJEKTUMLISTA[card]
+            for (const key in object) {
+                if (key === "nev"){
+                    txt+=`<div class="card-header">${object[key]}</div>`;
+                }
+                else{
+                    txt+=`<div class="card-body">${object[key]}</div>`;
+                }
             }
-            else{
-                txt+=`<div class="card-body">${object[key]}</div>`;
-            }
-        }
-        txt+='</div>';
-        return txt
-        
+            
+            txt+='</div>';
+            txt+="<div class='container-fluid mt-3'>"
+            txt +='<div class="row">'
+            txt+='<div class="col p-3".col>'
+                txt+='<input type="button" id="bal" value="elözö" name=bal>'
+                txt+='<input type="button" id="alap" value="alap" name=jobb>'
+                txt+='<input type="button" id="job" value="következö" name=jobb>'
+            txt+='</div>'
+            ARTICLEELEM.html(txt);
 }
